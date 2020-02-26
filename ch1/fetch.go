a7f0a70ae447bd0f3d688b3d0c62ca09736ec29c
Christian Bernini <bernini@santander.com.br>
2020-02-26 18:36:58 -0300

    ch1/fetch.go ex1.7 a 1.9 feitos

---
+++ ch1/fetch.go
@@ -0,0 +1,28 @@
+// Fetch exibe o conteúdo encontrado em cada URL especificada
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch:  %v\n", err)
+			os.Exit(1)
+		} else {
+			log.Println(resp.Status)
+			io.Copy(os.Stdout, resp.Body)
+			resp.Body.Close()
+		}
+	}
+}
