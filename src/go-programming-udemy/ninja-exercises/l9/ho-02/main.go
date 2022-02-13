7c46a633cb0e4364230bb2c7f2ab476fe5045c97
Christian Bernini <cgbas1987@gmail.com>
2022-02-13 18:48:15 -0300

    l9 e2

---
+++ src/go-programming-udemy/ninja-exercises/l9/ho-02/main.go
@@ -0,0 +1,50 @@
+/*
+	This exercise will reinforce our understanding of method sets:
+		create a type person struct
+		attach a method speak to type person using a pointer receiver
+			*person
+		create a type human interface
+		to implicitly implement the interface, a human must have the speak method
+		create func “saySomething”
+		have it take in a human as a parameter
+		have it call the speak method
+		show the following in your code
+		you CAN pass a value of type *person into saySomething
+		you CANNOT pass a value of type person into saySomething
+		here is a hint if you need some help
+		https://play.golang.org/p/FAwcQbNtMG
+		Receivers Values
+		-----------------------------------------------
+		(t T) T and *T
+		(t *T) *T
+*/
+
+package main
+
+import "fmt"
+
+type person struct {
+	name string
+}
+
+type human interface {
+	speak() string
+}
+
+func main() {
+	p := person{
+		name: "Christian Silva",
+	}
+
+	saySomething(&p)
+	// Doesn't work
+	//saySomething(p)
+}
+
+func (p *person) speak() string {
+	return fmt.Sprintf("Hello my name is %s", p.name)
+}
+
+func saySomething(h human) {
+	fmt.Println(h.speak())
+}
